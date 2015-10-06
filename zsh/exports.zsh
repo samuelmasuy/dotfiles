f65580c14829887bcdaf12c0ab817d373825c0ff
Samuel Masuy <samuel.masuy@gmail.com>
2015-10-05 20:11:54 -0400

    go path

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,9 +1,10 @@
 export GOPATH=$HOME/go
+export GOROOT=/usr/local/opt/go/libexec
 export MAVEN_OPTS="-Xms256m -Xmx512m"
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_79.jdk/Contents/Home"
 export IDEA_JDK="/Library/Java/JavaVirtualMachines/jdk1.7.0_79.jdk/Contents/Home"
 
-export PATH="/usr/local/sbin:/usr/local/bin/python:/usr/local/bin/python-config:/Applications/VirtualBox.app/Contents/MacOS:/usr/local/bin:/Applications/SWI-Prolog.app/Contents/MacOS:$HOME/bin:/usr/local/heroku/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/gradle/gradle-2.4/bin::/usr/local/apache-maven/apache-maven-3.3.3/bin:$GOPATH/bin:$PATH"
+export PATH="/usr/local/sbin:/usr/local/bin/python:/usr/local/bin/python-config:/Applications/VirtualBox.app/Contents/MacOS:/usr/local/bin:/Applications/SWI-Prolog.app/Contents/MacOS:$HOME/bin:/usr/local/heroku/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/gradle/gradle-2.4/bin::/usr/local/apache-maven/apache-maven-3.3.3/bin:$GOPATH/bin:$GOROOT/bin:$PATH"
 export MANPATH="/usr/local/man:$MANPATH"
 
 export EDITOR="vim"
