36e7973d1aa41bbdfc8299da043f7239b0127db2
Samuel Masuy <samuel.masuy@gmail.com>
2020-12-23 12:19:26 -0500

    lazy load kubectl and helm completions

--- zsh/config/completions.zsh
+++ zsh/config/completions.zsh
@@ -1,5 +1,22 @@
-source <(kubectl completion zsh)
-source <(helm completion zsh)
+# source <(kubectl completion zsh)
+# source <(helm completion zsh)
+
+
+function kubectl() {
+    if ! type __start_kubectl >/dev/null 2>&1; then
+        source <(command kubectl completion zsh)
+    fi
+
+    command kubectl "$@"
+}
+
+function helm() {
+    if ! type __start_helm >/dev/null 2>&1; then
+        source <(command helm completion zsh)
+    fi
+
+    command helm "$@"
+}
 
 # autoload -U +X bashcompinit && bashcompinit
 complete -o nospace -C $(which terraform) terraform
