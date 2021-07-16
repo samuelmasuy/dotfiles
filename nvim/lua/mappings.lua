12390c840bed7b949911f43aefdeef7281ce0cb0
Samuel Masuy <samuel.masuy@gmail.com>
2021-07-16 00:23:09 -0400

    cleanup

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,54 +1,55 @@
+local api = vim.api
 -- General Mappings
 -- try to use ZZ
-vim.api.nvim_set_keymap("c", "wq", ":echo 'Use ZZ or :x'<CR>", {noremap = true})
+api.nvim_set_keymap("c", "wq", ":echo 'Use ZZ or :x'<CR>", {noremap = true})
 
 -- Disable un-VI keys.
-vim.api.nvim_set_keymap("n", "<up>", "<nop>", {noremap = true})
-vim.api.nvim_set_keymap("n", "<down>", "<nop>", {noremap = true})
-vim.api.nvim_set_keymap("i", "<up>", "<nop>", {noremap = true})
-vim.api.nvim_set_keymap("i", "<down>", "<nop>", {noremap = true})
-vim.api.nvim_set_keymap("i", "<left>", "<nop>", {noremap = true})
-vim.api.nvim_set_keymap("i", "<right>", "<nop>", {noremap = true})
+api.nvim_set_keymap("n", "<up>", "<nop>", {noremap = true})
+api.nvim_set_keymap("n", "<down>", "<nop>", {noremap = true})
+api.nvim_set_keymap("i", "<up>", "<nop>", {noremap = true})
+api.nvim_set_keymap("i", "<down>", "<nop>", {noremap = true})
+api.nvim_set_keymap("i", "<left>", "<nop>", {noremap = true})
+api.nvim_set_keymap("i", "<right>", "<nop>", {noremap = true})
 
 -- Left and right can switch buffers
-vim.api.nvim_set_keymap("n", "<left>", ":bprevious<CR>", {noremap = true})
-vim.api.nvim_set_keymap("n", "<right>", ":bnext<CR>", {noremap = true})
+api.nvim_set_keymap("n", "<left>", ":bprevious<CR>", {noremap = true})
+api.nvim_set_keymap("n", "<right>", ":bnext<CR>", {noremap = true})
 
 -- Up and down can switch quickfix list
-vim.api.nvim_set_keymap("n", "<up>", ":cprevious<CR>", {noremap = true})
-vim.api.nvim_set_keymap("n", "<down>", ":cnext<CR>", {noremap = true})
+api.nvim_set_keymap("n", "<up>", ":cprevious<CR>", {noremap = true})
+api.nvim_set_keymap("n", "<down>", ":cnext<CR>", {noremap = true})
 
 -- Quick Fold and Unfold.
--- vim.api.nvim_set_keymap("n", "<space>", "za", {noremap = true})
--- vim.api.nvim_set_keymap("v", "<space>", "zf", {noremap = true})
+-- api.nvim_set_keymap("n", "<space>", "za", {noremap = true})
+-- api.nvim_set_keymap("v", "<space>", "zf", {noremap = true})
 
 -- Make sure to be in the middle of the screen when searching.
-vim.api.nvim_set_keymap("n", "n", "nzzzv", {noremap = true})
-vim.api.nvim_set_keymap("n", "N", "Nzzzv", {noremap = true})
+api.nvim_set_keymap("n", "n", "nzzzv", {noremap = true})
+api.nvim_set_keymap("n", "N", "Nzzzv", {noremap = true})
 
 -- Easier indentation of code blocks.
-vim.api.nvim_set_keymap("v", "<", "<gv", {noremap = true})
-vim.api.nvim_set_keymap("v", ">", ">gv", {noremap = true})
+api.nvim_set_keymap("v", "<", "<gv", {noremap = true})
+api.nvim_set_keymap("v", ">", ">gv", {noremap = true})
 
 -- basic, why not before
-vim.api.nvim_set_keymap("n", ";", ":", {noremap = true})
-vim.api.nvim_set_keymap("n", ":", ";", {noremap = true})
-vim.api.nvim_set_keymap("v", ";", ":", {noremap = true})
-vim.api.nvim_set_keymap("v", ":", ";", {noremap = true})
+api.nvim_set_keymap("n", ";", ":", {noremap = true})
+api.nvim_set_keymap("n", ":", ";", {noremap = true})
+api.nvim_set_keymap("v", ";", ":", {noremap = true})
+api.nvim_set_keymap("v", ":", ";", {noremap = true})
 
 -- logical
-vim.api.nvim_set_keymap("n", "Y", "y$", {noremap = true})
+api.nvim_set_keymap("n", "Y", "y$", {noremap = true})
 
 -- Switch marks
-vim.api.nvim_set_keymap("n", "'", "`", {noremap = true})
-vim.api.nvim_set_keymap("n", "`", "'", {noremap = true})
+api.nvim_set_keymap("n", "'", "`", {noremap = true})
+api.nvim_set_keymap("n", "`", "'", {noremap = true})
 
 -- Don't move on *
-vim.api.nvim_set_keymap("n", "*", ":let stay_star_view = winsaveview()<cr>*:call winrestview(stay_star_view)<cr>", {noremap = true, silent = true})
+api.nvim_set_keymap("n", "*", ":let stay_star_view = winsaveview()<cr>*:call winrestview(stay_star_view)<cr>", {noremap = true, silent = true})
 
 -- Terminal mode
-vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", {noremap = true})
-vim.api.nvim_set_keymap("t", "<C-h>", "<C-\\><C-n><C-w>h", {noremap = true})
-vim.api.nvim_set_keymap("t", "<C-j>", "<C-\\><C-n><C-w>j", {noremap = true})
-vim.api.nvim_set_keymap("t", "<C-k>", "<C-\\><C-n><C-w>k", {noremap = true})
-vim.api.nvim_set_keymap("t", "<C-l>", "<C-\\><C-n><C-w>l", {noremap = true})
+api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", {noremap = true})
+api.nvim_set_keymap("t", "<C-h>", "<C-\\><C-n><C-w>h", {noremap = true})
+api.nvim_set_keymap("t", "<C-j>", "<C-\\><C-n><C-w>j", {noremap = true})
+api.nvim_set_keymap("t", "<C-k>", "<C-\\><C-n><C-w>k", {noremap = true})
+api.nvim_set_keymap("t", "<C-l>", "<C-\\><C-n><C-w>l", {noremap = true})
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,97 +1,94 @@
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
+local opt = vim.opt
 
-vim.opt.autowrite = true
-vim.opt.inccommand = 'nosplit'
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.softtabstop = 4
 
-vim.opt.laststatus = 2
-vim.opt.showcmd = true
+opt.autowrite = true
+opt.inccommand = 'nosplit'
+
+opt.laststatus = 2
+opt.showcmd = true
 -- Show the line that have been wrapped.
-vim.opt.showbreak = '↪ '
+opt.showbreak = '↪ '
 -- Completion options (select longest + show menu even if a single match is found)
--- vim.opt.completeopt = 'menuone,noselect'
+-- opt.completeopt = 'menuone,noselect'
 vim.cmd 'autocmd CompleteDone * pclose'
 -- Make Esc work faster.
-vim.opt.ttimeoutlen = 40
+opt.ttimeoutlen = 40
 -- Always shows 5 lines above/below the cursor.
-vim.opt.scrolloff = 5
+opt.scrolloff = 5
 -- " Increment decimal not octal numbers.
-vim.opt.nrformats=''
+opt.nrformats=''
 -- Horizontal split goes to the bottom.
-vim.opt.splitbelow = true
+opt.splitbelow = true
 -- Vertical split goes to the right.
-vim.opt.splitright = true
+opt.splitright = true
 -- Automatically reread changed files without asking me anything
-vim.opt.autoread = true
+opt.autoread = true
 -- speed up syntax highlighting
-vim.opt.cursorcolumn = false
-vim.opt.cursorline = false
+opt.cursorcolumn = false
+opt.cursorline = false
 
 --Showing line numbers and length.
--- vim.opt.relativenumber = true
+-- opt.relativenumber = true
 -- Show current line numbers.
-vim.opt.number = true
+opt.number = true
 -- Width of document.
-vim.opt.tw = 79
+opt.tw = 79
 -- Don't automatically wrap on load.
-vim.opt.wrap = false
-vim.opt.formatoptions:remove({
+opt.wrap = false
+opt.formatoptions:remove({
     't',                       -- Don't auto wrap text
     'o',                       -- Do not continue comments with 'o' or 'O'
     '2',                       -- ... that's just weird
 })
 
 -- How many undos
-vim.opt.undolevels = 1000
+opt.undolevels = 1000
 -- Undo file
-vim.opt.undofile = true
+opt.undofile = true
 
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+opt.ignorecase = true
+opt.smartcase = true
 
 -- Wildmode aka tab-completion options.
-vim.opt.wildmode = 'full'
+opt.wildmode = 'full'
 
 -- for argdo and bufdo without a trailing bang.
-vim.opt.hidden = true
+opt.hidden = true
 
 -- Disable backup and swap files.
-vim.opt.backup = false
-vim.opt.writebackup = false
-vim.opt.swapfile = false
+opt.backup = false
+opt.writebackup = false
+opt.swapfile = false
 
-vim.opt.mouse:remove({
+opt.mouse:remove({
     'a', -- Disable mouse click to go to position
 })
 
-vim.opt.showmode = false
+opt.showmode = false
 
--- if vim.fn.executable "rg" then
---   vim.opt.grepprg = 'rg\ --vimgrep'
---   vim.opt.grepformat = '%f:%l:%c:%m,%f:%l:%m'
--- end
-
-vim.opt.diffopt:append {
+opt.diffopt:append {
     'vertical',           -- Gdiff vertical split
     'algorithm:patience', -- Make diffing better: https://vimways.org/2018/the-power-of-diff/
     'indent-heuristic',
 }
 
 -- gnupg
-vim.opt.modeline = true
-vim.opt.modelines = 5
+opt.modeline = true
+opt.modelines = 5
 
 -- Better display for messages
--- vim.opt.cmdheight = 1
+-- opt.cmdheight = 1
 -- Smaller updatetime for CursorHold & CursorHoldI
-vim.opt.updatetime = 300
+opt.updatetime = 300
 
  -- don't give |ins-completion-menu| messages.
-vim.opt.shortmess     = vim.opt.shortmess + 'c'
+opt.shortmess     = opt.shortmess + 'c'
 
 -- " always show signcolumns
-vim.opt.signcolumn = 'yes'
+opt.signcolumn = 'yes'
 
 -- Colorize the 100th column if goes over.
 vim.cmd('highlight ColorColumn ctermbg=magenta')
