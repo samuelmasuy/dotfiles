3cc31ca9269dbd62c87c236c92e09c0be5244ec0
Samuel Masuy <samuel.masuy@gmail.com>
2022-11-24 21:36:17 -0500

    simpler screen
    Signed-off-by: Samuel Masuy <samuel.masuy@gmail.com>

--- nvim/.config/nvim/lua/sam/startup/screen.lua
+++ nvim/.config/nvim/lua/sam/startup/screen.lua
@@ -1,18 +1,49 @@
 -- originally authored by @AdamWhittingham
+local if_nil = vim.F.if_nil
+local fnamemodify = vim.fn.fnamemodify
+local filereadable = vim.fn.filereadable
 
 local path_ok, alpha = pcall(require, "alpha")
 if not path_ok then
 	return
 end
 
-local path_ok1, plenary_path = pcall(require, "plenary.path")
-if not path_ok1 then
-	return
-end
+----
+local leader = "SPC"
 
-local dashboard = require("alpha.themes.dashboard")
-local cdir = vim.fn.getcwd()
-local if_nil = vim.F.if_nil
+--- @param sc string
+--- @param txt string
+--- @param keybind string? optional
+--- @param keybind_opts table? optional
+local function button(sc, txt, keybind, keybind_opts)
+	local sc_ = sc:gsub("%s", ""):gsub(leader, "<leader>")
+
+	local opts = {
+		position = "left",
+		shortcut = "[" .. sc .. "] ",
+		cursor = 1,
+		-- width = 50,
+		align_shortcut = "left",
+		hl_shortcut = { { "Operator", 0, 1 }, { "Number", 1, #sc + 1 }, { "Operator", #sc + 1, #sc + 2 } },
+		shrink_margin = false,
+	}
+	if keybind then
+		keybind_opts = if_nil(keybind_opts, { noremap = true, silent = true, nowait = true })
+		opts.keymap = { "n", sc_, keybind, { noremap = false, silent = true, nowait = true } }
+	end
+
+	local function on_press()
+		local key = vim.api.nvim_replace_termcodes(keybind .. "<Ignore>", true, false, true)
+		vim.api.nvim_feedkeys(key, "t", false)
+	end
+
+	return {
+		type = "button",
+		val = txt,
+		on_press = on_press,
+		opts = opts,
+	}
+end
 
 local nvim_web_devicons = {
 	enabled = true,
@@ -34,30 +65,30 @@ local function icon(fn)
 	return nwd.get_icon(fn, ext, { default = true })
 end
 
-local function file_button(fn, sc, short_fn)
-	short_fn = short_fn or fn
+local function file_button(fn, sc, short_fn, autocd)
+	short_fn = if_nil(short_fn, fn)
 	local ico_txt
 	local fb_hl = {}
-
 	if nvim_web_devicons.enabled then
 		local ico, hl = icon(fn)
 		local hl_option_type = type(nvim_web_devicons.highlight)
 		if hl_option_type == "boolean" then
 			if hl and nvim_web_devicons.highlight then
-				table.insert(fb_hl, { hl, 0, 3 })
+				table.insert(fb_hl, { hl, 0, 1 })
 			end
 		end
 		if hl_option_type == "string" then
-			table.insert(fb_hl, { nvim_web_devicons.highlight, 0, 3 })
+			table.insert(fb_hl, { nvim_web_devicons.highlight, 0, 1 })
 		end
 		ico_txt = ico .. "  "
 	else
 		ico_txt = ""
 	end
-	local file_button_el = dashboard.button(sc, ico_txt .. short_fn, "<cmd>e " .. fn .. " <CR>")
+	local cd_cmd = (autocd and " | cd %:p:h" or "")
+	local file_button_el = button(sc, ico_txt .. short_fn, "<cmd>e " .. fn .. cd_cmd .. " <CR>")
 	local fn_start = short_fn:match(".*[/\\]")
 	if fn_start ~= nil then
-		table.insert(fb_hl, { "Comment", #ico_txt - 2, #fn_start + #ico_txt })
+		table.insert(fb_hl, { "Comment", #ico_txt - 2, #fn_start + #ico_txt - 2 })
 	end
 	file_button_el.opts.hl = fb_hl
 	return file_button_el
@@ -69,15 +100,15 @@ local mru_opts = {
 	ignore = function(path, ext)
 		return (string.find(path, "COMMIT_EDITMSG")) or (vim.tbl_contains(default_mru_ignore, ext))
 	end,
+	autocd = false,
 }
 
 --- @param start number
---- @param cwd string optional
---- @param items_number number optional number of items to generate, default = 10
+--- @param cwd string? optional
+--- @param items_number number? optional number of items to generate, default = 10
 local function mru(start, cwd, items_number, opts)
 	opts = opts or mru_opts
-	items_number = if_nil(items_number, 11)
-
+	items_number = if_nil(items_number, 10)
 	local oldfiles = {}
 	for _, v in pairs(vim.v.oldfiles) do
 		if #oldfiles == items_number then
@@ -90,31 +121,20 @@ local function mru(start, cwd, items_number, opts)
 			cwd_cond = vim.startswith(v, cwd)
 		end
 		local ignore = (opts.ignore and opts.ignore(v, get_extension(v))) or false
-		if (vim.fn.filereadable(v) == 1) and cwd_cond and not ignore then
+		if (filereadable(v) == 1) and cwd_cond and not ignore then
 			oldfiles[#oldfiles + 1] = v
 		end
 	end
-	local target_width = 39
 
 	local tbl = {}
 	for i, fn in ipairs(oldfiles) do
 		local short_fn
 		if cwd then
-			short_fn = vim.fn.fnamemodify(fn, ":.")
+			short_fn = fnamemodify(fn, ":.")
 		else
-			short_fn = vim.fn.fnamemodify(fn, ":~")
+			short_fn = fnamemodify(fn, ":~")
 		end
-
-		if #short_fn > target_width then
-			short_fn = plenary_path.new(short_fn):shorten(1, { -2, -1 })
-			if #short_fn > target_width then
-				short_fn = plenary_path.new(short_fn):shorten(1, { -1 })
-			end
-		end
-
-		local shortcut = tostring(i + start - 1)
-
-		local file_button_el = file_button(fn, shortcut, short_fn)
+		local file_button_el = file_button(fn, tostring(i + start - 1), short_fn, opts.autocd)
 		tbl[i] = file_button_el
 	end
 	return {
@@ -124,112 +144,76 @@ local function mru(start, cwd, items_number, opts)
 	}
 end
 
-local default_header = {
-	type = "text",
-	val = {
-		[[o]],
-		[[ o   ^__^]],
-		[[  o  (oo)\_______]],
-		[[     (__)\       )\/\]],
-		[[         ||----w |]],
-		[[         ||     ||]],
-	},
-	-- Cow startify
-	opts = {
-		position = "center",
-		hl = "function",
-		-- wrap = "overflow";
-	},
-}
+local function mru_title()
+	return "MRU " .. vim.fn.getcwd()
+end
 
--- local section_mru_cwd = {
--- 	type = "group",
--- 	val = {
--- 		{
--- 			type = "text",
--- 			val = "Recent files",
--- 			opts = {
--- 				hl = "SpecialComment",
--- 				shrink_margin = false,
--- 				position = "center",
--- 			},
--- 		},
--- 		{ type = "padding", val = 1 },
--- 		{
--- 			type = "group",
--- 			val = function()
--- 				return { mru(10, cdir) }
--- 			end,
--- 			opts = { shrink_margin = false },
--- 		},
--- 	},
--- }
-
-local section_mru = {
-	type = "group",
-	val = {
-		{
-			type = "text",
-			val = "Recent files",
-			opts = {
-				hl = "SpecialComment",
-				shrink_margin = false,
-				position = "center",
+local section = {
+	top_buttons = {
+		type = "group",
+		val = {
+			button("e", "  New file", "<cmd>ene<CR>"),
+			button("p", "  Projects", "<cmd>Telescope projects<CR>"),
+			button("u", "  Update plugins", "<cmd>PackerSync<CR>"),
+			button("q", "  Quit", "<cmd>qa<CR>"),
+		},
+	},
+	-- note about MRU: currently this is a function,
+	-- since that means we can get a fresh mru
+	-- whenever there is a DirChanged. this is *really*
+	-- inefficient on redraws, since mru does a lot of I/O.
+	-- should probably be cached, or maybe figure out a way
+	-- to make it a reference to something mutable
+	-- and only mutate that thing on DirChanged
+	mru = {
+		type = "group",
+		val = {
+			{ type = "padding", val = 1 },
+			{ type = "text", val = "MRU", opts = { hl = "SpecialComment" } },
+			{ type = "padding", val = 1 },
+			{
+				type = "group",
+				val = function()
+					return { mru(10) }
+				end,
 			},
 		},
-		{ type = "padding", val = 1 },
-		{
-			type = "group",
-			val = function()
-				return { mru(0) }
-			end,
-			opts = { shrink_margin = false },
+	},
+	mru_cwd = {
+		type = "group",
+		val = {
+			{ type = "padding", val = 1 },
+			{ type = "text", val = mru_title, opts = { hl = "SpecialComment", shrink_margin = false } },
+			{ type = "padding", val = 1 },
+			{
+				type = "group",
+				val = function()
+					return { mru(0, vim.fn.getcwd()) }
+				end,
+				opts = { shrink_margin = false },
+			},
 		},
-	},
-}
-
-local buttons = {
-	type = "group",
-	val = {
-		{ type = "text", val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
-		{ type = "padding", val = 1 },
-		dashboard.button("e", "  New file", "<cmd>ene<CR>"),
-		-- dashboard.button("f", "  Find file", "<cmd>Telescope find_files<CR>"),
-		-- dashboard.button("SPC F", "  Live grep"),
-		dashboard.button("p", "  Projects", "<cmd>Telescope projects<CR>"),
-		-- dashboard.button("c", "  Configuration", "<cmd>e ~/.config/nvim/init.lua <CR>"),
-		dashboard.button("u", "  Update plugins", "<cmd>PackerSync<CR>"),
-		dashboard.button("q", "  Quit", "<cmd>qa<CR>"),
-	},
-	position = "center",
-}
-
-local fortune = {
-	type = "text",
-	val = require("sam.startup.fortune")(),
-	opts = {
-		position = "center",
-		hl = "group",
-		max_width = 54,
 	},
 }
 
 local config = {
 	layout = {
 		{ type = "padding", val = 1 },
-		fortune,
-		default_header,
+		section.top_buttons,
+		section.mru_cwd,
+		section.mru,
 		{ type = "padding", val = 1 },
-		buttons,
-		-- { type = "padding", val = 1 },
-		-- section_mru_cwd,
-		{ type = "padding", val = 1 },
-		section_mru,
 	},
 	opts = {
-		margin = 5,
+		margin = 0,
+		redraw_on_resize = false,
 		setup = function()
-			vim.cmd("autocmd alpha_temp DirChanged * lua require('alpha').redraw()")
+			vim.api.nvim_create_autocmd("DirChanged", {
+				pattern = "*",
+				callback = function()
+					require("alpha").redraw()
+				end,
+			})
 		end,
 	},
 }
