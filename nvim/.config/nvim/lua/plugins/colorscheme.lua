d1340036794516074739b4c33459d8bddeb24fcd
Samuel Masuy <samuel.masuy@gmail.com>
2023-09-15 21:18:40 -0400

    try kanagawa colorscheme
    Signed-off-by: Samuel Masuy <samuel.masuy@gmail.com>

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -18,8 +18,69 @@ return {
 	-- "folke/tokyonight.nvim"
 	-- 'tjdevries/colorbuddy.vim'
 	-- 'tjdevries/gruvbuddy.nvim'
+
+	{
+		"rebelot/kanagawa.nvim",
+		config = function()
+			require("kanagawa").setup({
+				compile = true, -- enable compiling the colorscheme
+				undercurl = true, -- enable undercurls
+				commentStyle = { italic = true },
+				functionStyle = {},
+				keywordStyle = { italic = true },
+				statementStyle = { bold = true },
+				typeStyle = {},
+				transparent = false, -- do not set background color
+				dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+				terminalColors = true, -- define vim.g.terminal_color_{0,17}
+				colors = { -- add/modify theme and palette colors
+					palette = {},
+					theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+				},
+				overrides = function(colors) -- add/modify highlights
+					return {}
+				end,
+				theme = "wave", -- Load "wave" theme when 'background' option is not set
+				background = { -- map the value of 'background' option to a theme
+					dark = "wave", -- try "dragon" !
+					light = "lotus",
+				},
+			})
+
+			local opt = vim.opt
+			local cmd = vim.cmd
+
+			opt.background = "dark"
+			opt.termguicolors = true
+
+			cmd("highlight ColorColumn ctermbg=magenta")
+			vim.fn.matchadd("ColorColumn", "\\%101v", 100)
+
+			-- undercurl
+			cmd([[
+	       hi SpellBad     gui=undercurl term=undercurl cterm=undercurl
+	     ]])
+			cmd("colorscheme kanagawa")
+
+			vim.diagnostic.config({
+				virtual_text = {
+					-- source = "always",  -- Or "if_many"
+					-- prefix = "●", -- Could be '■', '▎', 'x'
+				},
+				severity_sort = true,
+				float = {
+					source = "always",
+					border = "rounded",
+					-- source = "always", -- Or "if_many"
+				},
+			})
+		end,
+	},
+
 	{
 		"navarasu/onedark.nvim",
+		enabled = false,
+
 		config = function()
 			local opt = vim.opt
 			local cmd = vim.cmd
@@ -47,8 +108,8 @@ return {
 
 			-- undercurl
 			cmd([[
-        hi SpellBad     gui=undercurl term=undercurl cterm=undercurl
-      ]])
+	       hi SpellBad     gui=undercurl term=undercurl cterm=undercurl
+	     ]])
 
 			-- local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 			-- for type, icon in pairs(signs) do
--- nvim/.config/nvim/lua/plugins/line.lua
+++ nvim/.config/nvim/lua/plugins/line.lua
@@ -1,23 +1,25 @@
 return {
 	{
 		"nvim-lualine/lualine.nvim", -- inspired from evil_lualine: https://gist.github.com/hoob3rt/b200435a765ca18f09f83580a606b878
-		dependencies = {
-			"navarasu/onedark.nvim",
-		},
+		-- dependencies = {
+		-- 	"navarasu/onedark.nvim",
+		-- },
 		config = function()
-			-- local onedark_line = require'lualine.themes.onedark'
 			local lualine = require("lualine")
+
 			-- https://github.com/navarasu/onedark.nvim/blob/master/lua/onedark/colors.lua
-			local colors = require("onedark.colors")
+			-- local colors = require("onedark.colors")
+			local colors = require("kanagawa.colors").setup().palette
+
 			-- local colors = require('gruvbox.colors')
-			local theme = "onedark"
+			local theme = "kanagawa"
 
 			local diagnostics = {
 				"diagnostics",
 				sources = { "nvim_lsp" },
 				symbols = { error = " ", warn = " ", info = " " },
-				color_error = colors.red,
-				color_warn = colors.yellow,
+				color_error = colors.autumnRed,
+				color_warn = colors.autumnYellow,
 				-- color_info = colors.cyan,
 			}
 
@@ -39,14 +41,14 @@ return {
 					end
 				end,
 				icon = " ",
-				color = { fg = colors.dark_cyan },
+				color = { fg = colors.waveAqua1 },
 				condition = function()
 					return #vim.lsp.get_active_clients() > 0 and vim.fn.winwidth(0) > 40
 				end,
 			}
 
-			local location = { "location", color = { fg = colors.dark_yellow, gui = "bold" } }
-			local filename = { "filename", color = { fg = colors.fg, gui = "bold" } }
+			local location = { "location", color = { fg = colors.roninYellow, gui = "bold" } }
+			local filename = { "filename", color = { fg = colors.fujiWhite, gui = "bold" } }
 			local filetype = {
 				"filetype",
 				condition = function()
