d6854fdb27020d7d5c0b6e2f3156c482ebc14510
Samuel Masuy <samuel.masuy@gmail.com>
2024-01-23 22:30:06 -0500

    add description to gitsigns mappings

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -50,51 +50,51 @@ return {
           end
 
           -- Navigation
-          map("n", "]c", function()
+          map("n", "]h", function()
             if vim.wo.diff then
-              return "]c"
+              return "]h"
             end
             vim.schedule(function()
               gs.next_hunk()
             end)
             return "<Ignore>"
-          end, { expr = true })
+          end, { expr = true, desc = "Next hunk" })
 
-          map("n", "[c", function()
+          map("n", "[h", function()
             if vim.wo.diff then
-              return "[c"
+              return "[h"
             end
             vim.schedule(function()
               gs.prev_hunk()
             end)
             return "<Ignore>"
-          end, { expr = true })
+          end, { expr = true, desc = "Previous hunk" })
 
           -- Actions
-          map("n", "<leader>hs", gs.stage_hunk)
-          map("n", "<leader>hr", gs.reset_hunk)
+          map("n", "<leader>hs", gs.stage_hunk, { desc = "Stage hunk" })
+          map("n", "<leader>hr", gs.reset_hunk, { desc = "Reset hunk" })
           map("v", "<leader>hs", function()
             gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-          end)
+          end, { desc = "Stage hunk" })
           map("v", "<leader>hr", function()
             gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-          end)
-          map("n", "<leader>hS", gs.stage_buffer)
-          map("n", "<leader>hu", gs.undo_stage_hunk)
-          map("n", "<leader>hR", gs.reset_buffer)
-          map("n", "<leader>hp", gs.preview_hunk)
+          end, { desc = "Reset hunk" })
+          map("n", "<leader>hS", gs.stage_buffer, { desc = "Stage buffer" })
+          map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "Undo stage hunk" })
+          map("n", "<leader>hR", gs.reset_buffer, { desc = "Reset buffer" })
+          map("n", "<leader>hp", gs.preview_hunk, { desc = "Preview hunk" })
           map("n", "<leader>hb", function()
             gs.blame_line({ full = true })
-          end)
-          map("n", "<leader>tb", gs.toggle_current_line_blame)
+          end, { desc = "Blame line" })
+          map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "Toggle blame line" })
           map("n", "<leader>hd", gs.diffthis)
           map("n", "<leader>hD", function()
             gs.diffthis("~")
-          end)
-          map("n", "<leader>td", gs.toggle_deleted)
+          end, { desc = "Diff this against ancestor" })
+          map("n", "<leader>td", gs.toggle_deleted, { desc = "Toggle deleted" })
 
           -- Text object
-          map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+          -- map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
         end,
       })
     end,
