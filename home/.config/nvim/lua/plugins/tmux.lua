a31ac0b270e5a7b2d26f67e1964a5d73dcf1228f
Samuel Masuy <samuel.masuy@gmail.com>
2024-01-25 21:27:33 -0500

    try smart-splits
    Signed-off-by: Samuel Masuy <samuel.masuy@gmail.com>

--- home/.config/nvim/lua/plugins/tmux.lua
+++ home/.config/nvim/lua/plugins/tmux.lua
@@ -4,21 +4,82 @@ return {
     event = "VeryLazy",
   },
   {
-    "alexghergh/nvim-tmux-navigation",
-    event = "BufEnter",
+    "mrjones2014/smart-splits.nvim",
+    keys = {
+      -- window movement
+      {
+        "<C-h>",
+        function()
+          require("smart-splits").move_cursor_left()
+        end,
+      },
+      {
+        "<C-j>",
+        function()
+          require("smart-splits").move_cursor_down()
+        end,
+      },
+      {
+        "<C-k>",
+        function()
+          require("smart-splits").move_cursor_up()
+        end,
+      },
+      {
+        "<C-l>",
+        function()
+          require("smart-splits").move_cursor_right()
+        end,
+      },
+      {
+        "<A-h>",
+        function()
+          require("smart-splits").resize_left()
+        end,
+      },
+      {
+        "<A-j>",
+        function()
+          require("smart-splits").resize_down()
+        end,
+      },
+      {
+        "<A-k>",
+        function()
+          require("smart-splits").resize_up()
+        end,
+      },
+      {
+        "<A-l>",
+        function()
+          require("smart-splits").resize_right()
+        end,
+      },
+    },
     config = function()
-      local nvim_tmux_nav = require("nvim-tmux-navigation")
-
-      nvim_tmux_nav.setup({
-        disable_when_zoomed = false, -- defaults to false
+      require("smart-splits").setup({
+        disable_multiplexer_nav_when_zoomed = false,
       })
-
-      vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-      vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-      vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-      vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
-      vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
-      vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
     end,
   },
+  -- {
+  --
+  --   "alexghergh/nvim-tmux-navigation",
+  --
+  --   event = "BufEnter",
+  --   config = function()
+  --     local nvim_tmux_nav = require("nvim-tmux-navigation")
+  --
+  --     nvim_tmux_nav.setup({
+  --       disable_when_zoomed = false, -- defaults to false
+  --     })
+  --
+  --     vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+  --     vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+  --     vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+  --     vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+  --     vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+  --     vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+  --   end,
+  -- },
 }
