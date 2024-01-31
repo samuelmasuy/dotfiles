27f8bf7bfe58b7d587a79890f5093b1e8c345741
Samuel Masuy <samuel.masuy@gmail.com>
2024-01-30 23:02:09 -0500

    Clean up old colorscheme

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,8 @@
 return {
   {
     "rebelot/kanagawa.nvim",
+    priority = 1000,
+    lazy = false,
     enabled = true,
     config = function()
       require("kanagawa").setup({
@@ -32,75 +34,18 @@ return {
         -- end,
       })
 
-      local opt = vim.opt
-      local cmd = vim.cmd
+      vim.opt.background = "dark"
+      vim.opt.termguicolors = true
 
-      opt.background = "dark"
-      opt.termguicolors = true
-
-      cmd("highlight ColorColumn ctermbg=magenta")
+      vim.cmd.highlight("ColorColumn ctermbg=magenta")
       vim.fn.matchadd("ColorColumn", "\\%101v", 100)
 
       -- undercurl
-      cmd([[
+      vim.cmd([[
 	       hi SpellBad     gui=undercurl term=undercurl cterm=undercurl
 	     ]])
-      cmd("colorscheme kanagawa")
+      vim.cmd.colorscheme("kanagawa")
 
-      vim.diagnostic.config({
-        virtual_text = {
-          -- source = "always",  -- Or "if_many"
-          -- prefix = "●", -- Could be '■', '▎', 'x'
-        },
-        severity_sort = true,
-        float = {
-          source = "always",
-          border = "rounded",
-          -- source = "always", -- Or "if_many"
-        },
-      })
-    end,
-  },
-
-  {
-    "navarasu/onedark.nvim",
-    enabled = false,
-
-    config = function()
-      local opt = vim.opt
-      local cmd = vim.cmd
-
-      opt.background = "dark"
-      opt.termguicolors = true
-
-      -- Colorscheme
-      -- cmd [[colorscheme material]]
-      -- cmd [[colorscheme one-nvim]]
-      -- cmd [[colorscheme tokyonight]]
-      -- cmd [[colorscheme gruvbox]]
-
-      require("onedark").setup({
-        style = "dark",
-        diagnostics = {
-          undercurl = true, -- use undercurl instead of underline for diagnostics
-        },
-      })
-      require("onedark").load()
-
-      -- Colorize the 100th column if goes over.
-      cmd("highlight ColorColumn ctermbg=magenta")
-      vim.fn.matchadd("ColorColumn", "\\%101v", 100)
-
-      -- undercurl
-      cmd([[
-	       hi SpellBad     gui=undercurl term=undercurl cterm=undercurl
-	     ]])
-
-      -- local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-      -- for type, icon in pairs(signs) do
-      -- 	local hl = "DiagnosticSign" .. type
-      -- 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-      -- end
       vim.diagnostic.config({
         virtual_text = {
           -- source = "always",  -- Or "if_many"
