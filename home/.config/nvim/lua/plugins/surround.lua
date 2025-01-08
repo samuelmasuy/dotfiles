f4a403e65dcb65d6a96bd7f344e2d1529f900056
Samuel Masuy <samuel.masuy@gmail.com>
2025-01-08 00:25:36 -0500

    chore: cleanup surround plugin keys
    Signed-off-by: Samuel Masuy <samuel.masuy@gmail.com>

--- home/.config/nvim/lua/plugins/surround.lua
+++ home/.config/nvim/lua/plugins/surround.lua
@@ -1,14 +1,11 @@
 return {
   "kylechui/nvim-surround",
   keys = {
-    { "ys", mode = { "n", "v" } },
-    { "S", mode = { "n", "v" } },
-    { "ds", mode = { "n", "v" } },
-    { "cs", mode = { "n", "v" } },
-    { "cS", mode = { "n", "v" } },
-    { "yss", mode = { "n", "v" } },
-    { "yS", mode = { "n", "v" } },
-    { "ySS", mode = { "n", "v" } },
+    { "ys",  mode = { "n" }, desc = "Add surround" },
+    { "yss", mode = { "n" }, desc = "Add surround (line)" },
+    { "ds",  mode = { "n" }, desc = "Delete surround" },
+    { "cs",  mode = { "n" }, desc = "Change surround" },
+    { "S",   mode = { "v" }, desc = "Add surround" },
   },
   opts = {},
 }
