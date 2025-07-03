df1b00aa1bbde803c1ba67390997922e5e14722e
Samuel Masuy <samuel.masuy@gmail.com>
2025-07-03 12:11:08 -0400

    chore: clean up syntax plugins
    Signed-off-by: Samuel Masuy <samuel.masuy@gmail.com>

--- home/.config/nvim/lua/plugins/syntax.lua
+++ home/.config/nvim/lua/plugins/syntax.lua
@@ -1,26 +1,6 @@
 return {
   -- Syntax
-  { "ajorgensen/vim-markdown-toc", ft = "markdown" },
   { "modille/groovy.vim", ft = { "groovy", "Jenkinsfile" } },
   { "vim-scripts/haproxy", ft = "haproxy*" },
   { "chr4/nginx.vim", ft = { "nginx" } },
-  -- "mustache/vim-mustache-handlebars",
-  -- "justinmk/vim-syntax-extra",
-  -- express yourself well
-  { "rhysd/vim-grammarous", ft = { "text", "markdown" } },
-  {
-    "ron89/thesaurus_query.vim", -- synonym
-    keys = { "<leader>cs" },
-  },
-  { "chrisbra/unicode.vim", ft = { "text", "markdown" } },
-  -- all hashicorp goodies
-  {
-    -- "hashivim/vim-packer",
-    {
-      "hashivim/vim-terraform",
-      event = "VeryLazy",
-    },
-
-    -- "hashivim/vim-vaultproject",
-  },
 }
