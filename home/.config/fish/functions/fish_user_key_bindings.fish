a5285362bbb2a4ad9adbd7086e4cda49815a43f2
Samuel Masuy <samuel.masuy@gmail.com>
2025-10-02 09:41:12 -0400

    feat(fish): add keybinding to clear screen
    Signed-off-by: Samuel Masuy <samuel.masuy@gmail.com>

--- home/.config/fish/functions/fish_user_key_bindings.fish
+++ home/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,5 @@
 function fish_user_key_bindings
   # Unsuspend a job and bring it to the foreground
   bind -M insert \cz 'fg 2>/dev/null; commandline -f repaint'
+  bind -M insert \cb 'clear-screen'
 end
