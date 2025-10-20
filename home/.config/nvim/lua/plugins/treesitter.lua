50b7a0329e0cce71faa474100b28e246c2aae788
Samuel Masuy <samuel.masuy@gmail.com>
2025-10-20 10:50:47 -0400

    feat(nvim): use latest version of nvim-treesitter
    Signed-off-by: Samuel Masuy <samuel.masuy@gmail.com>

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -1,120 +1,77 @@
 return {
-  -- TREE SITTER:
-  { -- Highlight, edit, and navigate code
+  {
     "nvim-treesitter/nvim-treesitter",
-    -- event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-      },
-    },
+    -- dependencies = {
+    --   {
+    --     "nvim-treesitter/nvim-treesitter-textobjects",
+    --   },
+    -- },
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
 
-    -- [[ Config ]]
-    -- See `:help nvim-treesitter`
     config = function()
-      -- [[ Configure Treesitter ]]
-      -- See `:help nvim-treesitter`
-      require("nvim-treesitter.configs").setup({
-        modules = {},
-        -- Add languages to be installed here that you want installed for treesitter
-        ensure_installed = {
-          "bash",
-          "cmake",
-          "csv",
-          "dockerfile",
-          "fish",
-          "git_config",
-          "git_rebase",
-          "gitattributes",
-          "gitcommit",
-          "gitignore",
-          "go",
-          "gomod",
-          "groovy",
-          "hcl",
-          "html",
-          "http",
-          "javascript",
-          "jq",
-          "json",
-          "json5",
-          "lua",
-          "make",
-          "markdown",
-          "markdown_inline",
-          "python",
-          "query",
-          "regex",
-          "toml",
-          "typescript",
-          "vim",
-          "vimdoc",
-          "yaml",
-        },
-        ignore_install = {},
-        auto_install = true,
-        sync_install = false,
+      local ensure_install = {
+        "bash",
+        "cmake",
+        "csv",
+        "dockerfile",
+        "fish",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "go",
+        "gomod",
+        "groovy",
+        "hcl",
+        "html",
+        "http",
+        "javascript",
+        "jq",
+        "json",
+        "json5",
+        "lua",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "toml",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+      }
+      -- convert ensure_install to a list of strings
+      require("nvim-treesitter").install(ensure_install)
 
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = true,
-          disable = { "gitcommit" },
-        },
-        indent = { enable = true },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<c-space>",
-            node_incremental = "<c-space>",
-            scope_incremental = "<c-s>",
-            node_decremental = "<c-backspace>",
-          },
-        },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ["aa"] = "@parameter.outer",
-              ["ia"] = "@parameter.inner",
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              ["]m"] = "@function.outer",
-              ["]c"] = "@class.outer",
-            },
-            goto_next_end = {
-              ["]M"] = "@function.outer",
-              ["]C"] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[m"] = "@function.outer",
-              ["[c"] = "@class.outer",
-            },
-            goto_previous_end = {
-              ["[M"] = "@function.outer",
-              ["[C"] = "@class.outer",
-            },
-          },
-          swap = {
-            enable = false,
-            -- swap_next = {
-            --   ["<leader>p"] = "@parameter.inner",
-            -- },
-            -- swap_previous = {
-            --   ["<leader>P"] = "@parameter.inner",
-            -- },
-          },
-        },
+      local syntax_on = {}
+      for _, v in ipairs(ensure_install) do
+        syntax_on[v] = true
+      end
+      syntax_on["gitcommit"] = false
+
+      local group = vim.api.nvim_create_augroup("custom-treesitter", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        group = group,
+        callback = function(args)
+          local bufnr = args.buf
+          local ok, parser = pcall(vim.treesitter.get_parser, bufnr)
+          if not ok or not parser then
+            return
+          end
+          pcall(vim.treesitter.start)
+
+          local ft = vim.bo[bufnr].filetype
+          -- Enable syntax highlighting on all filetype from ensured list except gitcommit
+
+          if syntax_on[ft] then
+            vim.bo[bufnr].syntax = "on"
+          end
+        end,
       })
     end,
   },
