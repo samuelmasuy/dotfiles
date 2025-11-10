49371a45f9824044a8e50fe2b688553a1c95fd3c
Samuel Masuy <samuel.masuy@gmail.com>
2025-11-10 11:46:09 -0500

    chore(mise): neovim from official remote
    Signed-off-by: Samuel Masuy <samuel.masuy@gmail.com>

--- home/.config/fish/functions/upgrade-neovim-nightly.fish
+++ home/.config/fish/functions/upgrade-neovim-nightly.fish
@@ -1,3 +1,3 @@
-function upgrade-neovim-nightly --wraps='mise uninstall neovim:neovim@nightly && mise install neovim:neovim@nightly' --description 'alias upgrade-neovim-nightly=mise uninstall neovim:neovim@nightly && mise install neovim:neovim@nightly'
-  mise uninstall neovim:neovim@nightly && mise install neovim:neovim@nightly $argv
+function upgrade-neovim-nightly --wraps='mise uninstall neovim@nightly && mise install neovim@nightly' --description 'alias upgrade-neovim-nightly=mise uninstall neovim@nightly && mise install neovim@nightly'
+  mise uninstall neovim@nightly && mise install neovim@nightly $argv
 end
