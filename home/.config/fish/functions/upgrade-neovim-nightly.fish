9ac7eaac6956c732d496b34dd38cd55cbedcdd00
Samuel Masuy <samuel.masuy@gmail.com>
2025-11-21 22:06:00 -0500

    chore: add daily upgrade script
    Signed-off-by: Samuel Masuy <samuel.masuy@gmail.com>

--- home/.config/fish/functions/upgrade-neovim-nightly.fish
+++
@@ -1,3 +0,0 @@
-function upgrade-neovim-nightly --wraps='mise uninstall neovim@nightly && mise install neovim@nightly' --description 'alias upgrade-neovim-nightly=mise uninstall neovim@nightly && mise install neovim@nightly'
-  mise uninstall neovim@nightly && mise install neovim@nightly $argv
-end
