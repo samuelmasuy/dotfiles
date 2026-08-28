bb682eaa37050f747b32a35c8aea55bfca89269d
Samuel Masuy <samuel.masuy@gmail.com>
2026-08-27 22:08:17 -0400

    refactor(nvim): replace nvim-surround with mini.surround
    - surround.lua: kylechui/nvim-surround -> echasnovski/mini.surround (consistent with mini.files/mini.align/mini.trailspace)
    - Keep same keymaps ys/yss/ds/cs and S in visual via remap for muscle memory
    - Consolidates to mini suite, less overhead

--- home/.config/nvim/lua/plugins/surround.lua
+++ home/.config/nvim/lua/plugins/surround.lua
@@ -1,10 +1,31 @@
 return {
-  "kylechui/nvim-surround",
-  keys = {
-    { "ys",  mode = { "n" }, desc = "Add surround" },
-    { "yss", mode = { "n" }, desc = "Add surround (line)" },
-    { "ds",  mode = { "n" }, desc = "Delete surround" },
-    { "cs",  mode = { "n" }, desc = "Change surround" },
-    { "S",   mode = { "v" }, desc = "Add surround" },
+  {
+    "echasnovski/mini.surround",
+    version = "*",
+    opts = {
+      mappings = {
+        add = "ys",
+        delete = "ds",
+        find = "sf",
+        find_left = "sF",
+        highlight = "sh",
+        replace = "cs",
+        update_n_lines = "sn",
+        suffix_last = "l",
+        suffix_next = "n",
+      },
+    },
+    keys = {
+      { "ys", desc = "Add surround" },
+      { "yss", desc = "Add surround (line)" },
+      { "ds", desc = "Delete surround" },
+      { "cs", desc = "Change surround" },
+      { "S", mode = "v", desc = "Add surround" },
+    },
+    config = function(_, opts)
+      require("mini.surround").setup(opts)
+      -- Keep S as visual add for muscle memory (mini.surround uses ys in visual)
+      vim.keymap.set("x", "S", "ys", { remap = true, desc = "Add surround (visual)" })
+    end,
   },
 }
